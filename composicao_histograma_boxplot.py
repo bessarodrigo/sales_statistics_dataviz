be09669c1b68d3f55f4358a0ac60521f8824065f
Rodrigo Bessa <136742263+bessarodrigo@users.noreply.github.com>
2025-01-21 20:38:47 -0300

    updates_def

--- composicao_histograma_boxplot.py
+++ composicao_histograma_boxplot.py
@@ -66,3 +66,23 @@ def composicao_histograma_boxplot(dataframe, coluna, intervalos="auto", titulo="
             print(f"Erro ao salvar o arquivo {caminho_arquivo}: {e}")
 
     plt.show()
+
+import numpy as np
+from scipy.stats import mode, skew, kurtosis
+
+# Função para calcular as estatísticas
+def calcular_estatisticas(amostra):
+    moda_result = mode(amostra, keepdims=True)  # Garantir que a moda é retornada como array
+    estatisticas = {
+        "Média": np.mean(amostra),
+        "Mediana": np.median(amostra),
+        "Moda": moda_result.mode[0] if len(moda_result.mode) > 0 else np.nan,
+        "Variância": np.var(amostra, ddof=1),
+        "Desvio Padrão": np.std(amostra, ddof=1),
+        "Assimetria": skew(amostra),
+        "Curtose": kurtosis(amostra, fisher=False),  # fisher=False retorna a curtose de momento
+        "1º Quartil": np.percentile(amostra, 25),
+        "2º Quartil (Mediana)": np.percentile(amostra, 50),
+        "3º Quartil": np.percentile(amostra, 75)
+    }
+    return estatisticas
